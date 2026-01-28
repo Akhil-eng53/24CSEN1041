97762e401c6a8d2441237334c247acdb2a131277
Akhil-eng53 <abasava@student.gitam.edu>
2026-01-28 11:28:43 +0530

    Add files via upload

---
+++ random username password .py
@@ -0,0 +1,20 @@
+import random
+import string
+import secrets
+
+# -------- Username Generator --------
+name = input("Enter your name: ")
+
+num = random.randint(100, 999)   # random number
+username = name.lower() + str(num)
+
+# -------- Password Generator --------
+length = 12
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(characters) for _ in range(length))
+
+# -------- Output --------
+print("\nAccount Created Successfully!")
+print("Username:", username)
+print("Password:", password)
